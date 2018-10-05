5e6db5a05286d9020ce8ffe0a6ff44bff5dc12cc
jingwenyu <jyu57@hawk.iit.edu>
2018-10-05 14:37:52 -0500

    Changed to relative layout and added a location listAdapter

--- ChinatownApp/app/src/main/java/com/example/ipro/chinatownapp/LocationActivity.java
+++ ChinatownApp/app/src/main/java/com/example/ipro/chinatownapp/LocationActivity.java
@@ -7,20 +7,26 @@ public class LocationActivity {
     private ArrayList<String> location2;
     private ArrayList<String> location3;
     private ArrayList<String> location4;
+    private ArrayList<String> location5;
 
-    private void location1() {
-
-    }
-    private void location2() {
+    private void setLocation1() {
 
     }
-    private void location3() {
+    private void setLocation2() {
+
+    }
+    private void setLocation3() {
 
     }
 
-    private void location4() {
+    private void setLocation4() {
 
     }
+
+    private void setLocation5() {
+
+    }
+
 
     public ArrayList<String> getLocation1() {
         return location1;
@@ -36,17 +42,15 @@ public class LocationActivity {
 
     public ArrayList<String> getLocation4() {return location4; }
 
+    public ArrayList<String> getLocation5() {return location5; }
+
 
     public LocationActivity() {
         location1 = new ArrayList();
         location2 = new ArrayList();
         location3 = new ArrayList();
         location4 = new ArrayList();
-        location1();
-        location2();
-        location3();
-        location4();
-
+        location5 = new ArrayList();
     }
 
 }
