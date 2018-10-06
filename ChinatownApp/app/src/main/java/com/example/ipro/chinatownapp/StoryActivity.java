cec4b2ed42732500a7f6288a5575a41471338d94
jingwenyu <jyu57@hawk.iit.edu>
2018-10-06 14:27:50 -0500

    Changed to relative layout, added location listAdapter

---
+++ ChinatownApp/app/src/main/java/com/example/ipro/chinatownapp/StoryActivity.java
@@ -0,0 +1,22 @@
+package com.example.ipro.chinatownapp;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class StoryActivity extends AppCompatActivity {
+
+    SharedPreferences sp;
+    SharedPreferences.Editor editor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story);
+
+        sp = getSharedPreferences("language", Context.MODE_PRIVATE);
+        editor = sp.edit();
+        
+    }
+}
