c52adab90db9818a6da8de0015ccc4a619d0e8f5
jingwenyu <jyu57@hawk.iit.edu>
2018-11-14 12:05:45 -0600

    Added basic location page layout

--- ChinatownApp/app/src/main/java/com/example/ipro/chinatownapp/LocationActivity.java
+++ ChinatownApp/app/src/main/java/com/example/ipro/chinatownapp/LocationActivity.java
@@ -10,22 +10,40 @@ public class LocationActivity {
 
     private void setupPhotoPool() {
         mPhotoPool.add(R.drawable.home_icon); //use the same picture for now, and will replace after we have the contents
-        /*mPhotoPool.add(R.drawable.location_1);
-        mPhotoPool.add(R.drawable.location_1);
-        mPhotoPool.add(R.drawable.location_1);*/
+        mPhotoPool.add(R.drawable.home_icon);
+        mPhotoPool.add(R.drawable.home_icon);
+        mPhotoPool.add(R.drawable.home_icon);
+        mPhotoPool.add(R.drawable.home_icon);
+        mPhotoPool.add(R.drawable.home_icon);
+        mPhotoPool.add(R.drawable.home_icon);
+        mPhotoPool.add(R.drawable.home_icon);
+        mPhotoPool.add(R.drawable.home_icon);
+        mPhotoPool.add(R.drawable.home_icon);
     }
     private void setupLocation() {
         mLocation.add(R.string.location_1); //the location descriptions are in the strings.xml
-        /*mLocation.add(R.string.location_2);
+        mLocation.add(R.string.location_2);
         mLocation.add(R.string.location_3);
-        mLocation.add(R.string.location_4);*/
+        mLocation.add(R.string.location_4);
+        mLocation.add(R.string.location_5);
+        mLocation.add(R.string.location_6);
+        mLocation.add(R.string.location_7);
+        mLocation.add(R.string.location_8);
+        mLocation.add(R.string.location_9);
+        mLocation.add(R.string.location_10);
     }
 
     private void setupPhotoHDPool() {
         mPhotoHdPool.add(R.drawable.home_icon);
-        /*mPhotoHdPool.add(R.drawable.location_1);
-        mPhotoHdPool.add(R.drawable.location_1);
-        mPhotoHdPool.add(R.drawable.location_1);*/
+        mPhotoHdPool.add(R.drawable.home_icon);
+        mPhotoHdPool.add(R.drawable.home_icon);
+        mPhotoHdPool.add(R.drawable.home_icon);
+        mPhotoHdPool.add(R.drawable.home_icon);
+        mPhotoHdPool.add(R.drawable.home_icon);
+        mPhotoHdPool.add(R.drawable.home_icon);
+        mPhotoHdPool.add(R.drawable.home_icon);
+        mPhotoHdPool.add(R.drawable.home_icon);
+        mPhotoHdPool.add(R.drawable.home_icon);
     }
 
     public ArrayList<Integer> getmPhotoHdPool() {
